657dde8263857a16b26839ae5510e7335b45f143
Magnus Møller <magnus108@me.com>
2018-04-28 14:42:21 +0000

    added 404

--- site.hs
+++ site.hs
@@ -20,7 +20,7 @@ main = hakyllWith config $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.markdown", "contact.markdown"]) $ do
+    match (fromList ["about.markdown", "contact.markdown", "404.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
